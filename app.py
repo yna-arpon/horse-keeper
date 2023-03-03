fe6cfad30464f847009a3c18fcd72503cce4a44d
Yna Arpon <ynaarpon4@gmail.com>
2023-03-03 14:39:24 -0700

    model of cough instance

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///coughData.db'
+db = SQLAlchemy(app)
+
+class cough(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # specific id of each instance
+    coughCount = db.Column(db.Integer) # holds the number of coughs
+    dateCreated = db.Column(db.DateTime, default=datetime.date) # when the cough data is downloaded 
+    dataName = db.Column(db.String(100), default='Cough Count on ' + datetime.now().strftime("%d/%m/%Y %H:%M:%S")) 
+        # default name of cough data instance - user can change
+    
 
 @app.route('/')
 def index():
