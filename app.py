3d344acc97d4fa6b246c4341b64ae8e009854fc7
Yna Arpon <ynaarpon4@gmail.com>
2023-03-03 18:36:04 -0700

    linked css, js, html files

--- app.py
+++ app.py
@@ -14,9 +14,13 @@ class cough(db.Model):
         # default name of cough data instance - user can change
     
 
-@app.route('/')
-def index():
+@app.route('/', methods=['POST'])
+def home():
     return render_template('home.html')
+
+@app.route('/history')
+def history():
+    return render_template('history.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
