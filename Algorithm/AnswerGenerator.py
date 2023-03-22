8ef7726a387fd09c0ed61e3719fe23f574975818
T-bendig <teagan.bendig@gmail.com>
2023-03-22 12:20:06 -0600

    Finished? answer generator
    need to double check some things

--- Algorithm/AnswerGenerator.py
+++ Algorithm/AnswerGenerator.py
@@ -12,16 +12,18 @@ class AnswerGen:
         self.trues[2] = self.accel_min_answer(self)
         self.trues[3] = self.accel_time_answer(self)
         result = np.add(self.trues[0], self.trues[1], self.trues[2], self.trues[3])
-        cough = 0
+        cough = [[],[]]
         for x in result:
-            if result > 2 :
-                cough + cough + 1
+            if result > 2 and x in self.trues[1] == 1:
+                cough[0] += 1
+                cough[1].append(self.data2[3][np.where(x)])
+
         return cough   
 
     def audio_answer (self):
         audio_result = []
         for x in self.data1[2]:
-            if self.data1[2] > 2*self.data1[1]:
+            if x > 2*self.data1[1][np.where(x)]:
                 audio_result.append(1)
             else:
                 audio_result.append(0)
@@ -30,7 +32,7 @@ class AnswerGen:
     def accel_max_answer (self):
         accel_max_result = []
         for x in self.data2[2]:
-            if self.data2[2] > 2*self.data2[1]:
+            if x > 2*self.data2[1][np.where(x)]:
                 accel_max_result.append(1)
             else:
                 accel_max_result.append(0)
@@ -39,7 +41,7 @@ class AnswerGen:
     def accel_min_answer (self):
         accel_min_result = []
         for x in self.data2[5]:
-            if self.data2[5] > 2*self.data2[4]:
+            if x > 2*self.data2[4][np.where(x)]:
                 accel_min_result.append(1)
             else:
                 accel_min_result.append(0)
@@ -47,5 +49,9 @@ class AnswerGen:
 
     def accel_time_answer (self):
         accel_time_result = []
-
+        for x, y in zip(self.data2[3],self.data2[6]):
+            if (x-y) == 1:
+                accel_time_result.append(1)
+            else:
+                accel_time_result.append(0)
         return accel_time_result
