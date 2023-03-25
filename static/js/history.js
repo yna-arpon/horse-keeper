07003c5b9f380067f3bb256f9ea10cedbf2da7ad
TaniaRizwan <rizwantania03@gmail.com>
2023-03-25 17:24:22 -0600

    initial functionality to sortbtn

--- static/js/history.js
+++ static/js/history.js
@@ -3,4 +3,12 @@
 
 
 // Sort by Button
-document.getElementById('sortbyBtn').addEventListener('click', sort, false)
+document.getElementById('sortbyBtn').addEventListener('click', tableSort, false)
+function tableSort()    {
+    // this function will sort the history table in the following ways:
+    // Ascending cough
+    // Descending cough
+    // time 
+    // data size 
+    alert("You have pressed the button. Currently it does nothing.")
+}
