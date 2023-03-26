274211618889cf39ef4749fa3dfc28efd45d186a
Yna Arpon <ynaarpon4@gmail.com>
2023-03-26 00:49:23 -0600

    let users download data, analyzing dialog, results dialog

--- static/js/home.js
+++ static/js/home.js
@@ -3,28 +3,67 @@
 
 // Global variables ------------------------------------------------------------------------------------------ 
 
-fileSelected = false;
+
 
 // DOM Objects ---------------------------------------------------------------------------------------------
-let selectBtn = document.getElementById('selectFileBtn');
 let uploadBtn = document.getElementById('uploadDataBtn');
+let accData = document.getElementById('accData');
+let audioData = document.getElementById('audioData');
+let loadingDialog = document.getElementById('loadingDialog');
+let coughCountDialog = document.getElementById('coughCountDialog');
+let coughCountVal = document.getElementById('coughCountVal');
+let coughDataHeadingName = document.getElementById('coughDataName');
+let backHomeBtn = document.getElementById('backHome');
+let toHistoryBtn = document.getElementById('toHistory')
 
 // Setup ------------------------------------------------------------------------------------------
 
-// --------- Checking if file is selected or not
-if (fileSelected) { // If we have a file selected, disable select btn
-    selectBtn.disabled = true;
-} else { // If we don't have a file selected, disable upload btn 
-    uploadBtn.disabled = true;
-}
+
 
 // Event Listeners ------------------------------------------------------------------------------------------
 
 // --------- When user clicks upload button
 uploadBtn.addEventListener('click', (_event) => {
-    // If file has been selected upload it
-    if (fileSelected) {
-        uploadReminder.style.display = 'flex'
-    // Otherwise show the reminder    
-    }
+    initializeAnalysis();
+});
+
+// --------- When user clicks back button on dialog
+backHomeBtn.addEventListener('click', (_event) => {
+    window.location.href = '/';
 })
+
+// --------- When user clicks history button on dialog
+toHistoryBtn.addEventListener('click', (_event) => {
+    window.location.href = '/history';
+})
+
+// Helper Functions ------------------------------------------------------------------------------------------
+
+// --------- Checking if files are selected or not - users can't upload until 2 files are selected
+setInterval(checkFileInput, 500); // checks every 500ms if files are selected
+
+function checkFileInput() {
+    // if (accData.files.length <= 0 || audioData.files.length <= 0 ) {
+    //     uploadBtn.disabled = true;
+    // } else {
+    //     uploadBtn.disabled = false;
+    // }
+}
+
+function initializeAnalysis() {
+    loadingDialog.showModal();
+
+    setTimeout(showResults, 3000);
+}
+
+function showResults() {
+    loadingDialog.close();
+    coughCountDialog.showModal();
+    coughCountDialog.style.display = 'flex';
+    
+    let coughCount = 13; // replace with a function that returns algorithm results
+    let coughDataName = 'Cough Count' // replace with a function that returns default name
+    
+    coughDataHeadingName.innerHTML = coughDataName;
+    coughCountVal.innerHTML = coughCount;
+}
