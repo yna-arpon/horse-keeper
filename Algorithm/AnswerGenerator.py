279be76d55c90449fe33e800252a2575eefc79f3
T-bendig <teagan.bendig@gmail.com>
2023-03-27 17:20:47 -0600

    fixed answer generator
    fixed double counting

--- Algorithm/AnswerGenerator.py
+++ Algorithm/AnswerGenerator.py
@@ -13,16 +13,18 @@ class AnswerGen:
         self.trues[3] = self.accel_time_answer(self)
         result = np.add(self.trues[0], self.trues[1], self.trues[2], self.trues[3])
         cough = [[],[]]
+        z = 0
         for x in result:
-            if result > 2 and x in self.trues[1] == 1:
+            if result > 2 and x in self.trues[1] == 1 and x != z:
                 cough[0] += 1
                 cough[1].append(self.data2[3][np.where(x)])
+                z=x
         return cough   
 
     def audio_answer (self):
         audio_result = []
         for x in self.data1[2]:
-            if x > 2*self.data1[1][np.where(x)] and audio_result[x] != audio_result[x-1]:
+            if x > 2*self.data1[1][np.where(x)]:
                 audio_result.append(1)
             else:
                 audio_result.append(0)
@@ -31,7 +33,7 @@ class AnswerGen:
     def accel_max_answer (self):
         accel_max_result = []
         for x in self.data2[2]:
-            if x > 2*self.data2[1][np.where(x)] and accel_max_result[x] != accel_max_result[x-1]:
+            if x > 2*self.data2[1][np.where(x)]:
                 accel_max_result.append(1)
             else:
                 accel_max_result.append(0)
@@ -40,7 +42,7 @@ class AnswerGen:
     def accel_min_answer (self):
         accel_min_result = []
         for x in self.data2[5]:
-            if x > 2*self.data2[4][np.where(x)] and accel_min_result[x] != accel_min_result[x-1]:
+            if x > 2*self.data2[4][np.where(x)]:
                 accel_min_result.append(1)
             else:
                 accel_min_result.append(0)
@@ -49,7 +51,7 @@ class AnswerGen:
     def accel_time_answer (self):
         accel_time_result = []
         for x, y in zip(self.data2[3],self.data2[6]):
-            if (x-y) < 2 and (x-y) > 1 and accel_time_result[x] != accel_time_result[x-1]:
+            if (x-y) < 2 and (x-y) > 1:
                 accel_time_result.append(1)
             else:
                 accel_time_result.append(0)
