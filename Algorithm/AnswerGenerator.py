4794771cc2eeeba77283864a64cd5561a37d804c
HadenSch <75541875+HadenSch@users.noreply.github.com>
2023-03-28 20:55:57 -0600

    fixing answer generator

--- Algorithm/AnswerGenerator.py
+++ Algorithm/AnswerGenerator.py
@@ -4,55 +4,88 @@ class AnswerGen:
     def __init__(self, audiodata, acceldata):
         self.data1 = audiodata
         self.data2 = acceldata
-        self.trues = [[],[],[],[]] # [[max audio],[max accel],[min],[time between max and min]]
+        self.trues = [[],[],[],[]] # [[max audio],[max accel],[min accel],[time between max and min]]
+        self.cough = [[],[]]
 
-    def answer (self):  
-        self.trues[0] = self.audio_answer(self)
-        self.trues[1] = self.accel_max_answer(self)
-        self.trues[2] = self.accel_min_answer(self)
-        self.trues[3] = self.accel_time_answer(self)
-        result = np.add(self.trues[0], self.trues[1], self.trues[2], self.trues[3])
-        cough = [[],[]]
+    def answer_return(self):
+        
+        self.audio_answer()
+        self.accel_max_answer()
+        self.accel_min_answer()
+        self.accel_time_answer()
+        self.answer()
+
+    def answer(self):  
+
+        result = self.trues[0] + self.trues[1] + self.trues[2] + self.trues[3]
+
         z = 0
         for x in result:
             if result > 2 and x in self.trues[1] == 1 and x != z:
-                cough[0] += 1
-                cough[1].append(self.data2[3][np.where(x)])
-                z=x
-        return cough   
+                self.cough[0] += 1
+                self.cough[1].append(self.data2[3][np.where(x)])
+                z=x   
 
-    def audio_answer (self):
+    def audio_answer(self):
+
         audio_result = []
+
         for x in self.data1[2]:
-            if x > 2*self.data1[1][np.where(x)]:
+
+            if x > 1.2*self.data1[1][np.where(self.data1[2] == x)[0][0]]:
                 audio_result.append(1)
+
             else:
                 audio_result.append(0)
-        return audio_result        
 
-    def accel_max_answer (self):
+        print(audio_result) #works
+
+        self.trues[0] = audio_result        
+
+    def accel_max_answer(self):
+
         accel_max_result = []
+
         for x in self.data2[2]:
-            if x > 2*self.data2[1][np.where(x)]:
+
+            if x > 1.2*self.data2[1][np.where(self.data2[2] == x)[0][0]]:
                 accel_max_result.append(1)
+
             else:
                 accel_max_result.append(0)
-        return accel_max_result
+
+        print(accel_max_result) #works
+
+        self.trues[1] = accel_max_result
    
-    def accel_min_answer (self):
+    def accel_min_answer(self):
+
         accel_min_result = []
+
         for x in self.data2[5]:
-            if x > 2*self.data2[4][np.where(x)]:
+
+            if x < 0.8*self.data2[4][np.where(self.data2[5] == x)[0][0]]:
                 accel_min_result.append(1)
+
             else:
                 accel_min_result.append(0)
-        return  accel_min_result    
 
-    def accel_time_answer (self):
+        print(accel_min_result) #works
+
+        self.trues[2] = accel_min_result    
+
+    def accel_time_answer(self):
+
         accel_time_result = []
+
         for x, y in zip(self.data2[3],self.data2[6]):
-            if (x-y) < 2 and (x-y) > 1:
+
+            if (x-y) < 2000 and (x-y) > 1000:
                 accel_time_result.append(1)
+
             else:
                 accel_time_result.append(0)
-        return accel_time_result
+
+        print(accel_time_result) #works
+
+        self.trues[3] = accel_time_result
--- Algorithm/MainRunner.py
+++ Algorithm/MainRunner.py
@@ -29,8 +29,8 @@ def main():
     for i in range(0, len(audio_data[0])//5000):
 
         #slice the audio data
-        audio_slice = audio_data[0][x:x+9999:10] #the number 1 can be changed to 2 (or larger) which skips over every other data point (this can be used to speed up the algorithm)
-        accel_slice = accel_data[0][x:x+9999:10]
+        audio_slice = audio_data[0][x:x+9999:1] #the number 1 can be changed to 2 (or larger) which skips over every other data point (this can be used to speed up the algorithm)
+        accel_slice = accel_data[0][x:x+9999:1]
 
         #make calculation class for audio slices
         audio_calculations = audio_data_calculator(audio_slice)
@@ -45,37 +45,28 @@ def main():
         #append info to audio_data (in the correct area)
         audio_data[1].append(audio_calculations.audio_returnarr[0])
         audio_data[2].append(audio_calculations.audio_returnarr[1])
-        audio_data[3].append(audio_calculations.audio_returnarr[2])
+        audio_data[3].append(audio_calculations.audio_returnarr[2] + x)
 
         accel_data[1].append(accel_calculations.accel_returnarr[0])
         accel_data[2].append(accel_calculations.accel_returnarr[2])
-        accel_data[3].append(accel_calculations.accel_returnarr[3])
+        accel_data[3].append(accel_calculations.accel_returnarr[3] + x)
         accel_data[4].append(accel_calculations.accel_returnarr[1])
         accel_data[5].append(accel_calculations.accel_returnarr[4])
-        accel_data[6].append(accel_calculations.accel_returnarr[5])
+        accel_data[6].append(accel_calculations.accel_returnarr[5] + x)
 
         x += 5000
 
-    print("This is audio array", audio_data)
-    #print("This is acclerometer array", accel_data)
+    #print("This is audio array", audio_data)
+    print("This is accelerometer array", accel_data)
+    print("This is a min avg array", accel_data[1])
+    print("This is a global min array", accel_data[4])
 
     #Get Answers
-    for j in range(0, len(audio_data[1])):
+    #make calculation class for audio slices
+    cough_answers = AnswerGen(audio_data, accel_data)
+    cough_answers.answer_return()
 
-        #slice the audio data calculations
-        audio_calc_slice = []
-        audio_calc_slice.append(audio_data[1][j])
-        audio_calc_slice.append(audio_data[2][j])
-        audio_calc_slice.append(audio_data[3][j])
-
-        accel_calc_slice = []
-        accel_calc_slice.append(accel_data[1][j])
-        accel_calc_slice.append(accel_data[2][j])
-        accel_calc_slice.append(accel_data[3][j])
-        accel_calc_slice.append(accel_data[4][j])
-        accel_calc_slice.append(accel_data[5][j])
-        accel_calc_slice.append(accel_data[6][j])
-    
+        
 
     # print(cough_num[0])
     # for x in cough_num[1]:
