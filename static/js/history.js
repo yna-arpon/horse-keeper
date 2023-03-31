c347484ac72b319d91cc94fc16c3f4a5ea2fa21d
TaniaRizwan <rizwantania03@gmail.com>
2023-03-31 11:43:41 -0600

    final sort button

--- static/js/history.js
+++ static/js/history.js
@@ -3,16 +3,6 @@
 
 
 // Sort by Button
-document.getElementById('sortbyBtn').addEventListener('click', tableSort, false)
-function tableSort()    {
-    // this function will sort the history table in the following ways:
-    // Ascending cough
-    // Descending cough
-    // time 
-    // data size 
-    alert("Sorted by Date")
-    
-}
 
 function sortNames() {
     var sortOption = document.getElementById("sortbyBtn").value;
