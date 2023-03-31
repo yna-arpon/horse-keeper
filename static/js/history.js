dae44730097c30489310ee9ed590588eb5ce6c59
TaniaRizwan <rizwantania03@gmail.com>
2023-03-31 11:41:23 -0600

    history page table

--- static/js/history.js
+++ static/js/history.js
@@ -16,23 +16,27 @@ function tableSort()    {
 
 function sortNames() {
     var sortOption = document.getElementById("sortbyBtn").value;
-    var nameList = document.querySelectorAll('tr:not(:first-child)');
-  
-    var namesArray = Array.prototype.slice.call(nameList);
-    namesArray.sort(function(a, b) {
-      var nameA = a.innerText.toLowerCase();
-      var nameB = b.innerText.toLowerCase();
-  
-      if (sortOption === "asc") {
-        return nameA.localeCompare(nameB);
-      } else {
-        return nameB.localeCompare(nameA);
-      }
+    var rows = document.querySelectorAll('tr:not(:first-child)');
+    
+    // Sort the rows based on the name column
+    var sortedRows = Array.from(rows)
+      .sort((a, b) => {
+        var nameA = a.querySelector('.name').textContent.toLowerCase();
+        var nameB = b.querySelector('.name').textContent.toLowerCase();
+        return sortOption === 'asc' ? nameA.localeCompare(nameB) : nameB.localeCompare(nameA);
+      });
+    
+    // Append the sorted rows to the table body
+    var tableBody = rows[0].parentNode;
+    sortedRows.forEach(row => tableBody.appendChild(row));
+    
+    // Update the delete buttons to match the new row order
+    var deleteButtons = document.getElementsByClassName('deleteBtn');
+    Array.from(deleteButtons).forEach(button => {
+      var rowIndex = button.closest('tr').rowIndex - 1; // Subtract 1 to account for the header row
+      button.className = 'deleteBtn';
+      button.setAttribute('onclick', `DeleteRowFunction(this, ${rowIndex})`);
     });
-  
-    var parent = nameList[0].parentNode.parentNode;
-    for (var i = 0; i < namesArray.length; i++) {
-      parent.appendChild(namesArray[i]);
-    }
   }
+  
   
