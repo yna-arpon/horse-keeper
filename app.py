6d73ab9e262c06ab2698b83e55d9d86282532245
TaniaRizwan <rizwantania03@gmail.com>
2023-04-04 17:53:34 -0600

    removing dmy

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from Algorithm.MainRunner import main
+from algorithm.MainRunner import main
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///coughData.db'
 db = SQLAlchemy(app)
 
+with app.app_context():
+    db.create_all()
+
 class cough(db.Model):
     id = db.Column(db.Integer, primary_key=True) # specific id of each instance
     coughCount = db.Column(db.Integer) # holds the number of coughs
     dateCreated = db.Column(db.DateTime, default=datetime.now) # when the cough data is downloaded 
-    dataName = db.Column(db.String(100), default='Cough Count on ' + datetime.now().strftime("%d/%m/%Y %H:%M:%S")) 
+    dataName = db.Column(db.String(100), default='Cough Count on ' + datetime.now().strftime(" %H:%M:%S")) 
         # default name of cough data instance - user can change
 
     def __repr__(self):
        return '<Data %r>' % self.id
+
+
 
 
 @app.route('/', methods=['POST','GET'])
@@ -41,16 +46,10 @@ def home():
 @app.route('/history', methods=['GET'])  # GETTING OPERATIONAL ERRORS. WIP
 def history():
     coughData = cough.query.order_by(cough.dateCreated).all() # Looks at database in order created and grab all
-    # if request.method == 'DELETE':
-    #     cough_to_delete = cough.query.get_or_404(id)
-        
-    #     db.session.delete(cough_to_delete)
-    #     db.session.commit()
-
-    #     return redirect('/history')
-    # else:
     return render_template('history.html', coughData=coughData)
     
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True) 
